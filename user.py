b763f01c5ea51d6515eb298d9489b577b0a0db5a
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-05 08:51:17 +0300

    initializing the class user

--- user.py
+++ user.py
@@ -3,3 +3,10 @@ class User:
     '''
     The User class will contain user details
     '''
+
+    def __init__(self, login, password):
+        '''
+         Ensuring the user class has unique properties
+        '''
+        self.login = login
+        self.password = password
