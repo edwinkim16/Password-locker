a6ff6c3165b9d57d57971c0dcc83587f70a144df
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-05 09:45:20 +0300

    user creating unique credentials

--- password-locker.py
+++ password-locker.py
@@ -6,3 +6,11 @@ class Password:
     Create your paswords here
     """
     password_list = []  # list to store user  passwords
+
+    def _init_(self, account, username, password):
+        """
+        Allow the user to create instances of the class with unique details 
+        """
+        self.account = account
+        self.username = username
+        self.password = password
