ca11d041015302e29bde307356e76056bb0ae1ff
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-05 11:57:10 +0300

    where user will run their functions

--- run.py
+++ run.py
@@ -2,3 +2,22 @@
 from Password_Locker import Password
 from user import User
 import getpass  # to hide the pass
+
+def new_user(login, password):
+    """
+    This will create a new user every time they login
+    Args:
+        login - user name
+        password - the user's password
+    Return:
+        the new user instance
+    """
+    user = User(login, password)
+    return user
+
+def main():
+    """
+    This is where the user will run all their functions
+    """    
+
+
