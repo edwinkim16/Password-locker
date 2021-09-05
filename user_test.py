d347896a4fa8b3aa7f9f2dab3029e243c87341fd
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-05 09:06:53 +0300

    testing setup function

--- user_test.py
+++ user_test.py
@@ -5,3 +5,18 @@ class TestUser(unittest.TestCase):
     '''
     This class will contain all tests for the user class
     '''
+    def setUp(self):
+        '''
+        This will first create new user before each test
+        '''
+        self.new_user = User ("edwin","1998")
+
+    def test_init(self):
+        '''
+        This will validate whetehr the user is correctly registered
+        '''
+        self.assertEqual(self.new_user.login,"edwin")
+        self.assertEqual(self.new_user.password,"1998")
+
+if __name__ == '__main__':
+    unittest.main()        
